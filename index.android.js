6130f649067067c12db4d3eaf4d63ca4dcedea64
Yuzi <Yuzi@Yuzi.local>
2017-05-07 16:02:13 +0800

    delete

--- index.android.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-import {AppRegistry} from 'react-native';
-import AndroidPullToRefresh from './src/Main';
-
-AppRegistry.registerComponent('AndroidPullToRefresh', () => AndroidPullToRefresh);
--- index.ios.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- * @flow
- */
-import {AppRegistry} from 'react-native';
-import AndroidPullToRefresh from './src/index';
-
-AppRegistry.registerComponent('AndroidPullToRefresh', () => AndroidPullToRefresh);
